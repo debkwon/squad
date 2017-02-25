f4e9d7533a22d54a1f2fbef4fee8fcf2acd0ae99
Deborah Kwon <deborah.m.kwon@gmail.com>
2017-02-24 23:36:31 -0500

    add initial pod routes, closes #15

--- server/api.js
+++ server/api.js
@@ -6,7 +6,9 @@ const api = module.exports = require('express').Router()
 api
   .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
+  .use('/pod', require('./pod'))
   .use('/members', require('./members'))
+
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
